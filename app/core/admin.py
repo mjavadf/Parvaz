387e7938b42e9bc90da0879f60188c1b344a9ed4
mjavadf <javad.me1376@gmail.com>
2025-07-04 22:54:04 +0200

    Define admin interface for custom user model with display options

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,2 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for the user model"""
+
+    ordering = ["id"]
+    list_display = ["username", "email", "is_staff", "is_superuser"]
+    list_filter = ["is_staff", "is_superuser"]
+    search_fields = ["username", "email"]
