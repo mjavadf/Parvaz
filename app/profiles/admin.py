0b0d5478ffbd50700c837787be3cfe8b35c66305
mjavadf <javad.me1376@gmail.com>
2025-07-06 23:12:44 +0200

    Register admin models for Specialization, Language, TherapistProfile, and ClientProfile

--- app/profiles/admin.py
+++ app/profiles/admin.py
@@ -1,2 +1,22 @@
+from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Specialization)
+class SpecializationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Language)
+class LanguageAdmin(admin.ModelAdmin):
+    ordering = ("iso",)
+
+
+@admin.register(models.TherapistProfile)
+class TherapistProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ClientProfile)
+class ClientProfileAdmin(admin.ModelAdmin):
+    pass
