00e0b9ce278472e63c5415e839288859e1907a49
mjavadf <javad.me1376@gmail.com>
2025-07-07 14:28:43 +0200

    Enhance UserAdmin to include profile_type in list display and fieldsets

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,14 +1,53 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.utils.translation import gettext_lazy as _
 from core import models
 
 
 @admin.register(models.User)
 class UserAdmin(BaseUserAdmin):
-    """Define the admin pages for the user model"""
-
+    """
+    Define the admin pages for the user model.
+    """
     ordering = ["id"]
-    list_display = ["username", "email", "is_staff", "is_superuser"]
-    list_filter = ["is_staff", "is_superuser"]
-    search_fields = ["username", "email"]
+    list_display = ["username", "email", "profile_type", "is_staff", "is_superuser"]
+    
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "profile_type",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+    
+    fieldsets = (
+        (None, {"fields": ("username", "password", "profile_type",)}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    
+    
+    
